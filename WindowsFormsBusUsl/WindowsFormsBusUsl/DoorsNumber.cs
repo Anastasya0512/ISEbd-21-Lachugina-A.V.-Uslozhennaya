ccc2436862f86db91294bfeb5791fa538dfbec25
Анастасия Лачугина <cool.lachugina@mail.ru>
2020-11-02 21:48:03 +0400

    done

---
+++ WindowsFormsBusUsl/WindowsFormsBusUsl/DoorsNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsBus
+{
+    public enum DoorsNumber
+    {
+        Three=3,
+        Four,
+        Five
+    }
+}
